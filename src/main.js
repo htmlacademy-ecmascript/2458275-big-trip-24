01e62a8be73be5505d45536e7896a476d97e9ce1
KseniiaPan <panovskaia.ksenia74@gmail.com>
2024-09-01 22:04:52 +0300

    исправления в соответствии с автопроверкой

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-import { render } from './render.js';
-import Filters from './view/filtes-view.js';
 import FiltersPresenter from './presenter/filters-presenter.js';
 
 const tripMainElement = document.querySelector('.trip-main');
