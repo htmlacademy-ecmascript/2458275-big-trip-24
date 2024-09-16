13b4fd486e3a1a009f3678cd67ed48353b1dc94c
KseniiaPan <panovskaia.ksenia74@gmail.com>
2024-09-16 12:34:05 +0300

    подключит модель для точек маршрута

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import FiltersPresenter from './presenter/filters-presenter.js';
 import EventsPresenter from './presenter/events-presenter.js';
+import PointsModel from './model/points-model.js';
 
 const tripFiltersElement = document.querySelector(
   '.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
-
 const filtersPresenter = new FiltersPresenter({
   filtersContainer: tripFiltersElement,
 });
 
+const pointsModel = new PointsModel();
 const eventsPresenter = new EventsPresenter({
   eventsContainer: tripEventsElement,
+  pointsModel,
 });
 
 filtersPresenter.init();
--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -9,18 +9,21 @@ import { render, RenderPosition } from '../render.js';
 export default class EventsPresenter {
 
   eventsListComponent = new EventsListView();
-  constructor({ eventsContainer }) {
+  constructor({ eventsContainer, pointsModel }) {
     this.eventsContainer = eventsContainer;
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    this.eventsPoints = [...this.pointsModel.getPoints()];
+
     render(new SortView(), this.eventsContainer, RenderPosition.BEFOREEND);
     render(this.eventsListComponent, this.eventsContainer, RenderPosition.BEFOREEND);
 
     render(new EventEditView(), this.eventsListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new EventView(), this.eventsListComponent.getElement());
-    }
+    for (let i = 0; i < this.eventsPoints.length; i++) {
+      render(new EventView(this.eventsPoints[i]), this.eventsListComponent.getElement());
     }
   }
+}
