33defa4a3f08c86ffb53122fd3ce8e23495913c8
KseniiaPan <panovskaia.ksenia74@gmail.com>
2024-09-20 13:04:34 +0300

    создает заглушку для отсутствия событий

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -2,6 +2,7 @@ import SortView from '../view/sort-view.js';
 import EventsListView from '../view/events-list-view.js';
 import EventEditView from '../view/event-edit-view.js';
 import EventPointView from '../view/event-point-view.js';
+import NoEventView from '../view/no-event-view.js';
 import {isEscapeKey} from '../utils.js';
 
 
@@ -27,6 +28,10 @@ export default class EventsPresenter {
   init() {
     this.#eventsPoints = [...this.#pointsModel.getPoints()];
 
+    if (this.#eventsPoints.length === 0) {
+      render(new NoEventView(), this.#eventsContainer);
+      return;
+    }
     render(new SortView(), this.#eventsContainer);
     render(this.#eventsListComponent, this.#eventsContainer);
 
