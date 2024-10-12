2c07b11524f577a2bcd3720c014760b55e531203
KseniiaPan <panovskaia.ksenia74@gmail.com>
2024-10-12 15:52:19 +0300

    Добавит модель для фильтра.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import FiltersPresenter from './presenter/filters-presenter.js';
 import BoardPresenter from './presenter/board-presenter.js';
 
 import PointsModel from './model/points-model.js';
+import FilterModel from './model/filter-model.js';
 
 const tripFiltersElement = document.querySelector(
   '.trip-controls__filters');
 const tripEventsElement = document.querySelector('.trip-events');
 
 const pointsModel = new PointsModel();
+const filterModel = new FilterModel();
 
 
 const filtersPresenter = new FiltersPresenter({
